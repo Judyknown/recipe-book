2f32fad87d04811351b46aa2ca29bc7f87a7cec6
Judy Zhu <Judy.Zhu@Solera.com>
2025-09-30 18:34:17 +0800

    update on self runner's Build.yml

--- BOOTCAMP/BootCamp/obj/Debug/net8.0/BootCamp.AssemblyInfo.cs
+++ BOOTCAMP/BootCamp/obj/Debug/net8.0/BootCamp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BootCamp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57b01a5a7f6f3dc0dbc67dd87786ac853dfce926")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98aa7922b002447200c11285aa1e31eb4081b562")]
 [assembly: System.Reflection.AssemblyProductAttribute("BootCamp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BootCamp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
